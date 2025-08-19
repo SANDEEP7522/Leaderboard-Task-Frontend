616b59981456244651987aa11fbf7fa0d6998a43
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2025-08-19 19:15:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { Toaster } from '@/components/ui/toaster';
+import { AppRoutes } from '@/Routes';
+
+import { AppContextProvider } from './context/AppContextProvider';
+function App() {
+
+const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppContextProvider>
+        <AppRoutes />
+      </AppContextProvider>
+      <Toaster />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
